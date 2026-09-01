bcc76b20ae605ca9125893ae23b5457a72443e91
bzhan166 <bzhan166@ucr.edu>
2026-09-01 12:57:29 -0400

    change lorozeno tile to fit rtx 3080

--- src/cuda/lorenzo_tile_dim.h
+++ src/cuda/lorenzo_tile_dim.h
@@ -8,5 +8,5 @@
 // Valid values: 8, 16, 32 (power of 2; block has TileDim*TileDim threads,
 // so 32 → 1024 threads/block which is the hardware max).
 #ifndef LORENZO_TILE_DIM
-#define LORENZO_TILE_DIM 32
+#define LORENZO_TILE_DIM 16
 #endif
